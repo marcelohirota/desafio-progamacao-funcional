4b14cc29becabbe331a788edc33b9188c40226d0
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-05-14 13:16:05 +0930

    final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pandas as pd
 
 records = [
     {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
@@ -15,6 +16,45 @@ records = [
     {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000}
 ]
 
+# Calculo do custo por ligação
+def call_fee (start_time, end_time):
+    
+    start_time = datetime.fromtimestamp(start_time)
+    end_time = datetime.fromtimestamp(end_time)
+
+    if (int(start_time.hour) >= 6) and (int(end_time.hour) <= 22):
+        fee = 0.36 + (((end_time - start_time).seconds//60)*0.09)
+        return float(fee)
+    else:
+        return 0.36
+
+# Adição da coluna 'cost' dentro do records
+def get_costs(records):
+    for calls in records:
+        calls.update({'cost': call_fee(calls['start'], calls['end'])})
+    return records
 
 def classify_by_phone_number(records):
-    pass
+    
+    final_results = []
+
+    df_results = pd.DataFrame(get_costs(records))
+
+    group_results = df_results.groupby('source')['cost'].sum().reset_index().rename(columns={'cost':'total_cost'})\
+        .sort_values(by='total_cost', ascending=False)
+
+    sources = [source for source in group_results['source']]
+
+    totals = [total for total in group_results['total_cost']]
+
+    for results in zip(sources, totals):
+        final_results.append({'source': results[0], 'total_cost': round(results[1],2)})
+
+    return final_results
+
+
+
+
+
+
+
